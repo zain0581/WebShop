751b9edd925faf937914ea44df6516b03ad21636
Zain Ali <meharzain243@yahoo.com>
2024-04-17 09:42:25 +0200

    x

--- IMSWeb/IMSWeb/obj/Debug/net7.0/IMSWeb.AssemblyInfo.cs
+++ IMSWeb/IMSWeb/obj/Debug/net7.0/IMSWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMSWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee60f7e971655b2510f5e6b7bc56c56ac0d45dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+701a35cf41f1ddf1ec17a29c556aca02d2e3d563")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMSWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMSWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
